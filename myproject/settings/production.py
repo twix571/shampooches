c9f3f78173a8e6130dc3ab843839227275b3151f
twix571 <julianh571@yahoo.com>
2026-02-12 13:02:44 -0500

    fix: Set STATIC_ROOT to /app/staticfiles and ensure directory exists

--- myproject/settings/production.py
+++ myproject/settings/production.py
@@ -32,6 +32,12 @@ EMAIL_SUBJECT_PREFIX = '[Shampooches] '
 
 DEBUG = False
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = 'static/'
+STATIC_ROOT = Path('/app/staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WHITENOISE_USE_FINDERS = True
